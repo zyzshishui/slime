3b674111c0a55267cbe9650f7f6410b81295c9b8
Zilin Zhu <zhuzilinallen@gmail.com>
2025-07-10 02:52:18 +0000

    add config for qwen3-0.6B

---
+++ scripts/models/qwen3-0.6B.sh
@@ -0,0 +1,17 @@
+MODEL_ARGS=(
+   --swiglu
+   --num-layers 28
+   --hidden-size 1024
+   --ffn-hidden-size 3072
+   --num-attention-heads 16
+   --group-query-attention
+   --num-query-groups 8
+   --use-rotary-position-embeddings
+   --disable-bias-linear
+   --normalization "RMSNorm"
+   --norm-epsilon 1e-6
+   --rotary-base 1000000
+   --vocab-size 151936
+   --kv-channels 128
+   --qk-layernorm
+)
