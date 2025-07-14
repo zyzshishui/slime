af231f2da27ebd6c83dfd10a84a6a79963ffe37a
Chen-GX <1198597830@qq.com>
2025-07-14 17:05:10 +0800

    add support of reinforce++ with CP

--- slime/backends/megatron_utils/loss.py
+++ slime/backends/megatron_utils/loss.py
@@ -10,6 +10,7 @@ from slime.utils.ppo_utils import (
     compute_log_probs,
     compute_policy_loss,
     get_grpo_returns,
+    get_reinforce_plus_plus_returns,
     get_reinforce_plus_plus_baseline_advantages,
 )
 from slime.utils.distributed_utils import distributed_masked_whiten
@@ -120,6 +121,7 @@ def compute_advantages_and_returns(args):
     values: Union[None, list[torch.Tensor]] = get_local_storage("values")
     response_lengths: list[int] = get_local_storage("response_lengths")
     loss_masks: list[torch.Tensor] = get_local_storage("loss_masks")
+    total_lengths: list[int] = get_local_storage("total_lengths")
 
     if log_probs is None:
         return
@@ -150,6 +152,18 @@ def compute_advantages_and_returns(args):
         # TODO: is the copy necessary?
         advantages = [r for r in returns]
 
+    elif args.advantage_estimator == "reinforce_plus_plus":
+        returns = get_reinforce_plus_plus_returns(
+            rewards=rewards,
+            kl=kl,
+            loss_masks=loss_masks,
+            response_lengths=response_lengths,
+            total_lengths=total_lengths,
+            kl_coef=args.kl_coef,
+            gamma=args.gamma,
+        )
+        advantages = [r for r in returns]
+
     elif args.advantage_estimator == "reinforce_plus_plus_baseline":
         advantages = get_reinforce_plus_plus_baseline_advantages(
             rewards=rewards,
@@ -165,9 +179,30 @@ def compute_advantages_and_returns(args):
     # TODO: OpenRLHF always does advantages normalization but veRL doesn't seem to do it.
     if args.normalize_advantages:
         all_advs = torch.cat(advantages)
-        all_masks = torch.cat(loss_masks)
+        cp_size = mpu.get_context_parallel_world_size()
+        if cp_size == 1:
+            all_masks = torch.cat(loss_masks)
+        else:
+            mask_chunks = []
+            for i in range(len(loss_masks)):
+                full_mask = loss_masks[i]
+                total_len, response_len, prompt_len = total_lengths[i], response_lengths[i], total_lengths[i] - response_lengths[i]
+
+                _, _, _, my_offsets = get_logits_and_tokens_offset_with_cp(total_len, response_len)
+                
+                s_start, e_start = my_offsets[0][0] - prompt_len, my_offsets[0][1] - prompt_len
+                s_end, e_end = my_offsets[1][0] - prompt_len, my_offsets[1][1] - prompt_len
+                
+                mask_chunk = torch.cat([full_mask[s_start:e_start], full_mask[s_end:e_end]])
+                mask_chunks.append(mask_chunk)
+            all_masks = torch.cat(mask_chunks)
+
+        assert all_advs.size() == all_masks.size(), \
+            f"Shape mismatch before whitening: advantages {all_advs.size()}, masks {all_masks.size()}"
+
         whitened_advs_flat = distributed_masked_whiten(all_advs, all_masks, shift_mean=True)
-        advantages = list(torch.split(whitened_advs_flat, response_lengths))
+        chunk_lengths = [chunk.size(0) for chunk in advantages]
+        advantages = list(torch.split(whitened_advs_flat, chunk_lengths))
 
     set_local_storage("advantages", advantages)
     set_local_storage("returns", returns)
