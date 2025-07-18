ed60ddc0d94ec06bb6b33a105f59cb48d5927afc
Zilin Zhu <zhuzilinallen@gmail.com>
2025-07-18 08:26:32 +0000

    fix removed imports

--- slime/backends/megatron_utils/__init__.py
+++ slime/backends/megatron_utils/__init__.py
@@ -3,6 +3,8 @@ import logging
 from .actor import MegatronTrainRayActor
 from .arguments import _vocab_size_with_padding, parse_args, validate_args
 from .checkpoint import load_checkpoint, save_checkpoint
+from .initialize import init
+from .model import initialize_model_and_optimizer
 
 logging.getLogger().setLevel(logging.WARNING)
 
@@ -14,4 +16,6 @@ __all__ = [
     "save_checkpoint",
     "_vocab_size_with_padding",
     "MegatronTrainRayActor",
+    "init",
+    "initialize_model_and_optimizer",
 ]
