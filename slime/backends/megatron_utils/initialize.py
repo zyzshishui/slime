e0778525d354defabaffc99f6ac93ac6395b4902
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-07-20 17:44:24 +0800

    more (#82)

--- slime/backends/megatron_utils/initialize.py
+++ slime/backends/megatron_utils/initialize.py
@@ -79,6 +79,9 @@ def init(args):
     _initialize_distributed(args)
     _init_gloo_group()
 
+    # https://github.com/NVIDIA/Megatron-LM/issues/1563
+    assert np.__version__.startswith("1."), "Megatron does not support numpy 2.x"
+
     # Random seeds for reproducibility.
     if args.rank == 0:
         print("> setting random seeds to {} ...".format(args.seed))
