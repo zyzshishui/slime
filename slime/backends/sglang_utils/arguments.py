0de4253b986800d8efcf42e27173e13d451c80c4
Zilin Zhu <zhuzilinallen@gmail.com>
2025-08-03 07:04:57 -0700

    fix enable_ep_moe for sglang v0.4.10

--- slime/backends/sglang_utils/arguments.py
+++ slime/backends/sglang_utils/arguments.py
@@ -111,7 +111,9 @@ def add_sglang_arguments(parser):
 
 
 def validate_args(args):
-    # sglang
+    if getattr(args, "sglang_enable_ep_moe", False):
+        args.sglang_expert_parallel_size = args.rollout_num_gpus_per_engine
+
     args.sglang_tp_size = args.rollout_num_gpus_per_engine
     args.sglang_dp_size = args.sglang_data_parallel_size
     args.sglang_pp_size = args.sglang_pipeline_parallel_size
