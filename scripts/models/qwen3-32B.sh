1fc0fae8a89f10942c12146f87c467d88e445cd8
zlHuang <108509803+zlH518@users.noreply.github.com>
2025-08-30 20:24:21 +0800

    Create qwen3-32B.sh (#208)

---
+++ scripts/models/qwen3-32B.sh
@@ -0,0 +1,17 @@
+MODEL_ARGS=(
+   --swiglu
+   --num-layers 64
+   --hidden-size 5120
+   --ffn-hidden-size 25600
+   --num-attention-heads 64
+   --group-query-attention
+   --num-query-groups 8
+   --use-rotary-position-embeddings
+   --disable-bias-linear
+   --normalization "RMSNorm"
+   --norm-epsilon 1e-6
+   --rotary-base 1000000
+   --vocab-size 151936
+   --kv-channels 128
+   --qk-layernorm
+)
