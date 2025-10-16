d3c431f7e32f713763f90cf64d2a209770b928bb
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-10-16 12:00:52 +0800

    Super tiny refactor code for reuse (#505)

--- slime/ray/ray_actor.py
+++ slime/ray/ray_actor.py
@@ -1,20 +1,10 @@
-import ray
-from slime.utils.http_utils import is_port_available
+from slime.utils.misc import get_current_node_ip, get_free_port
 
 
 class RayActor:
     @staticmethod
     def _get_current_node_ip_and_free_port(start_port=10000, consecutive=1):
-        address = ray._private.services.get_node_ip_address()
-        # strip ipv6 address
-        address = address.strip("[]")
-
-        # find the port where port, port + 1, port + 2, ... port + consecutive - 1 are all available
-        port = start_port
-        while not all(is_port_available(port + i) for i in range(consecutive)):
-            port += 1
-
-        return address, port
+        return get_current_node_ip(), get_free_port(start_port=start_port, consecutive=consecutive)
 
     def get_master_addr_and_port(self):
         return self.master_addr, self.master_port
--- slime/utils/misc.py
+++ slime/utils/misc.py
@@ -1,4 +1,10 @@
 import importlib
+import subprocess
+from typing import Optional
+
+import ray
+
+from slime.utils.http_utils import is_port_available
 
 
 def load_function(path):
@@ -24,3 +30,31 @@ class SingletonMeta(type):
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
+
+
+def exec_command(cmd: str, capture_output: bool = False) -> Optional[str]:
+    print(f"EXEC: {cmd}", flush=True)
+    result = subprocess.run(
+        ["bash", "-c", cmd],
+        shell=False,
+        check=True,
+        capture_output=capture_output,
+        **(dict(text=True) if capture_output else {}),
+    )
+    if capture_output:
+        return result.stdout
+
+
+def get_current_node_ip():
+    address = ray._private.services.get_node_ip_address()
+    # strip ipv6 address
+    address = address.strip("[]")
+    return address
+
+
+def get_free_port(start_port=10000, consecutive=1):
+    # find the port where port, port + 1, port + 2, ... port + consecutive - 1 are all available
+    port = start_port
+    while not all(is_port_available(port + i) for i in range(consecutive)):
+        port += 1
+    return port
