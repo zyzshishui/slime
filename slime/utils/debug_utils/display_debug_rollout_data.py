94535e64d9fa630393babbe67467cc383fc6efd2
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-11-13 16:03:49 +0800

    Add script to recompute metrics for an existing run (#664)

--- slime/utils/debug_utils/display_debug_rollout_data.py
+++ slime/utils/debug_utils/display_debug_rollout_data.py
@@ -1,9 +1,13 @@
 import json
 from pathlib import Path
-from typing import Annotated
+from types import SimpleNamespace
+from typing import Annotated, List
 
 import torch
 import typer
+
+from slime.ray.rollout import _compute_metrics_from_samples
+from slime.utils.types import Sample
 
 _WHITELIST_KEYS = [
     "group_index",
@@ -21,25 +25,46 @@ _WHITELIST_KEYS = [
 def main(
     # Deliberately make this name consistent with main training arguments
     load_debug_rollout_data: Annotated[str, typer.Option()],
+    show_metrics: bool = True,
+    show_samples: bool = True,
+    category: List[str] = ["train", "eval"],
 ):
-    for rollout_id in range(100):
-        for path in [
-            load_debug_rollout_data.format(rollout_id=f"eval_{rollout_id}"),
-            load_debug_rollout_data.format(rollout_id=rollout_id),
-        ]:
-            path = Path(path)
-            if not path.exists():
-                continue
+    for rollout_id, path in _get_rollout_dump_paths(load_debug_rollout_data, category):
+        print("-" * 80)
+        print(f"{rollout_id=} {path=}")
+        print("-" * 80)
 
-            print("-" * 80)
-            print(f"{rollout_id=} {path=}")
-            print("-" * 80)
+        pack = torch.load(path)
+        sample_dicts = pack["samples"]
 
-            pack = torch.load(path)
-            samples = pack["samples"]
+        if show_metrics:
+            # TODO read these configs from dumps
+            args = SimpleNamespace(
+                advantage_estimator="grpo",
+                reward_key=None,
+                log_reward_category=None,
+            )
+            sample_objects = [Sample.from_dict(s) for s in sample_dicts]
+            # TODO make the function public
+            metrics = _compute_metrics_from_samples(args, sample_objects)
+            print("metrics", metrics)
 
-            for sample in samples:
+        if show_samples:
+            for sample in sample_dicts:
                 print(json.dumps({k: v for k, v in sample.items() if k in _WHITELIST_KEYS}))
+
+
+def _get_rollout_dump_paths(load_debug_rollout_data: str, categories: List[str]):
+    # may improve later
+    for rollout_id in range(1000):
+        for category in categories:
+            prefix = {
+                "train": "",
+                "eval": "eval_",
+            }[category]
+            path = Path(load_debug_rollout_data.format(rollout_id=f"{prefix}{rollout_id}"))
+            if path.exists():
+                yield rollout_id, path
 
 
 if __name__ == "__main__":
