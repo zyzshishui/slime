dd0aef44a55f461681d30c13ba84764dee2533ce
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-11-14 21:09:29 +0800

    Add tests for external rollout feature (#734)

---
+++ tests/test_external_rollout.py
@@ -0,0 +1,185 @@
+import multiprocessing
+import time
+
+import command_utils as U
+
+MODEL_NAME = "Qwen2.5-0.5B-Instruct"
+MODEL_TYPE = "qwen2.5-0.5B"
+NUM_GPUS = 2
+SGLANG_ROUTER_IP = "127.0.0.1"
+SGLANG_ROUTER_PORT = 31000
+SGLANG_ENGINE_IP = "127.0.0.1"
+SGLANG_ENGINE_PORT = 32000
+
+
+def prepare():
+    U.exec_command("mkdir -p /root/models /root/datasets")
+    U.exec_command(f"huggingface-cli download Qwen/Qwen2.5-0.5B-Instruct --local-dir /root/models/{MODEL_NAME}")
+    U.hf_download_dataset("zhuzilin/gsm8k")
+    U.convert_checkpoint(model_name=MODEL_NAME, model_type=MODEL_TYPE, num_gpus=NUM_GPUS)
+
+
+def execute():
+    ckpt_args = f"--hf-checkpoint /root/models/{MODEL_NAME}/ " f"--ref-load /root/{MODEL_NAME}_torch_dist/ "
+
+    rollout_args = (
+        "--prompt-data /root/datasets/gsm8k/train.parquet "
+        "--input-key messages "
+        "--label-key label "
+        "--apply-chat-template "
+        "--rollout-shuffle "
+        "--rm-type math "
+        f"--num-rollout {3000 if U.get_env_enable_infinite_run() else 250} "
+        "--rollout-batch-size 32 "
+        "--n-samples-per-prompt 8 "
+        "--rollout-max-response-len 1024 "
+        "--rollout-temperature 0.8 "
+        "--over-sampling-batch-size 64 "
+        "--dynamic-sampling-filter-path slime.rollout.filter_hub.dynamic_sampling_filters.check_reward_nonzero_std "
+        "--global-batch-size 256 "
+    )
+
+    eval_args = (
+        "--eval-interval 20 "
+        "--eval-prompt-data gsm8k /root/datasets/gsm8k/test.parquet "
+        "--n-samples-per-eval-prompt 1 "
+        "--eval-max-response-len 1024 "
+        "--eval-top-k 1 "
+    )
+
+    perf_args = (
+        "--tensor-model-parallel-size 1 "
+        "--sequence-parallel "
+        "--pipeline-model-parallel-size 1 "
+        "--context-parallel-size 1 "
+        "--expert-model-parallel-size 1 "
+        "--expert-tensor-parallel-size 1 "
+        # "--micro-batch-size 1 "
+        "--use-dynamic-batch-size "
+        "--max-tokens-per-gpu 9216 "
+    )
+
+    grpo_args = (
+        "--advantage-estimator grpo "
+        "--use-kl-loss "
+        "--kl-loss-coef 0.00 "
+        "--kl-loss-type low_var_kl "
+        "--entropy-coef 0.00 "
+        "--eps-clip 0.2 "
+        "--eps-clip-high 0.28 "
+    )
+
+    optimizer_args = (
+        "--optimizer adam "
+        "--lr 1e-6 "
+        "--lr-decay-style constant "
+        "--weight-decay 0.1 "
+        "--adam-beta1 0.9 "
+        "--adam-beta2 0.98 "
+    )
+
+    sglang_args = "--rollout-num-gpus-per-engine 2 " f"--sglang-mem-fraction-static 0.6 "
+
+    ci_args = (
+        "--ci-test "
+        "--ci-disable-kl-checker "
+        "--ci-metric-checker-key eval/gsm8k "
+        "--ci-metric-checker-threshold 0.55 "  # loose threshold at 250 step
+    )
+
+    misc_args = (
+        # default dropout in megatron is 0.1
+        "--attention-dropout 0.0 "
+        "--hidden-dropout 0.0 "
+        # should be good for model performance
+        "--accumulate-allreduce-grads-in-fp32 "
+        "--attention-softmax-in-fp32 "
+        # need to comment this when using model with MLA
+        "--attention-backend flash "
+        "--actor-num-nodes 1 "
+        f"--actor-num-gpus-per-node 2 "
+        # TODO support non-colocate (e.g. remove rollout engine resource occupation)
+        "--colocate "
+        # isolated-rollout related
+        "--rollout-external "
+        # TODO test multi-engine
+        f"--rollout-external-engine-addrs {SGLANG_ENGINE_IP}:{SGLANG_ENGINE_PORT} "
+        f"--sglang-router-ip {SGLANG_ROUTER_IP} "
+        f"--sglang-router-port {SGLANG_ROUTER_PORT} "
+    )
+
+    train_args = (
+        f"{ckpt_args} "
+        f"{rollout_args} "
+        f"{optimizer_args} "
+        f"{grpo_args} "
+        f"{U.get_default_wandb_args(__file__)} "
+        f"{perf_args} "
+        f"{eval_args} "
+        f"{sglang_args} "
+        f"{ci_args} "
+        f"{misc_args} "
+    )
+
+    U.execute_train(
+        train_args=train_args,
+        num_gpus=NUM_GPUS,
+        model_type=MODEL_TYPE,
+        before_ray_job_submit=_launch_background,
+    )
+
+
+def _launch_background():
+    # Here we use infra in slime, but real users will use their own infra
+    _launch_sglang_router()
+    _launch_sglang_engine()
+
+
+def _launch_sglang_router():
+    from sglang_router.launch_router import RouterArgs
+
+    from slime.utils.http_utils import run_router
+
+    print("launch_sglang_router", flush=True)
+    router_args = RouterArgs(
+        host=SGLANG_ROUTER_IP,
+        port=SGLANG_ROUTER_PORT,
+        balance_abs_threshold=0,
+        log_level="warn",
+    )
+    proc_router = multiprocessing.Process(
+        target=run_router,
+        args=(router_args,),
+    )
+    proc_router.daemon = True
+    proc_router.start()
+    time.sleep(3)
+    assert proc_router.is_alive()
+
+
+def _launch_sglang_engine():
+    import requests
+    from sglang.srt.server_args import ServerArgs
+
+    from slime.backends.sglang_utils.sglang_engine import launch_server_process
+
+    print("launch_sglang_engine", flush=True)
+    launch_server_process(
+        ServerArgs(
+            model_path=f"/root/models/{MODEL_NAME}/",
+            trust_remote_code=True,
+            enable_memory_saver=True,
+            host=SGLANG_ENGINE_IP,
+            port=SGLANG_ENGINE_PORT,
+            tp_size=2,
+        )
+    )
+
+    requests.post(
+        f"http://{SGLANG_ROUTER_IP}:{SGLANG_ROUTER_PORT}/add_worker?url=http://{SGLANG_ENGINE_IP}:{SGLANG_ENGINE_PORT}"
+    )
+
+
+if __name__ == "__main__":
+    prepare()
+    execute()
