59af349847065d7984a1aa9f743bd3a2ae44d578
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-11-15 10:14:54 +0800

    Support ue8m0 quantization from training to inference (#758)

--- slime/backends/megatron_utils/megatron_to_hf/__init__.py
+++ slime/backends/megatron_utils/megatron_to_hf/__init__.py
@@ -23,7 +23,14 @@ def quantize_param(name, weight, weight_block_size):
     FP8_MIN = torch.finfo(torch.float8_e4m3fn).min
     FP8_MAX = torch.finfo(torch.float8_e4m3fn).max
     if weight_block_size is not None:
-        qweight, scale = blockwise_cast_to_fp8_triton(weight, weight_block_size)
+        from sglang.srt.layers.quantization.fp8_utils import quant_weight_ue8m0, transform_scale_ue8m0
+        from sglang.srt.model_loader.utils import should_deepgemm_weight_requant_ue8m0
+
+        if should_deepgemm_weight_requant_ue8m0(weight_block_size=weight_block_size):
+            qweight, scale = quant_weight_ue8m0(weight, weight_block_size=weight_block_size)
+            scale = transform_scale_ue8m0(scale, mn=qweight.shape[-2])
+        else:
+            qweight, scale = blockwise_cast_to_fp8_triton(weight, weight_block_size)
         scale_name = name.replace(".weight", ".weight_scale_inv")
     else:
         # per tensor quant
