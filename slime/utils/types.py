5472cabd278bf78074b94b967473ed5d5e8af77f
Zilin Zhu <zhuzilinallen@gmail.com>
2025-11-21 14:18:12 +0800

    Turn SpecInfo into dict when serializing Sample (#855)

--- slime/utils/types.py
+++ slime/utils/types.py
@@ -54,16 +54,37 @@ class Sample:
             if self.spec_verify_ct > 0:
                 self.spec_accept_length = response_length / self.spec_verify_ct
 
+        def to_dict(self):
+            return {
+                "spec_accept_token_num": self.spec_accept_token_num,
+                "spec_draft_token_num": self.spec_draft_token_num,
+                "spec_verify_ct": self.spec_verify_ct,
+                "spec_accept_rate": self.spec_accept_rate,
+                "spec_accept_length": self.spec_accept_length,
+            }
+
+        @staticmethod
+        def from_dict(data: dict):
+            info = Sample.SpecInfo()
+            info.spec_accept_token_num = data.get("spec_accept_token_num", 0)
+            info.spec_draft_token_num = data.get("spec_draft_token_num", 0)
+            info.spec_verify_ct = data.get("spec_verify_ct", 0)
+            info.spec_accept_rate = data.get("spec_accept_rate", 0.0)
+            info.spec_accept_length = data.get("spec_accept_length", 0.0)
+            return info
+
     spec_info: SpecInfo = field(default_factory=SpecInfo)
 
     def to_dict(self):
         value = self.__dict__.copy()
         value["status"] = self.status.value
+        value["spec_info"] = self.spec_info.to_dict()
         return value
 
     @staticmethod
     def from_dict(data: dict):
         data["status"] = Sample.Status(data["status"])
+        data["spec_info"] = Sample.SpecInfo.from_dict(data.get("spec_info", {}))
         return Sample(**data)
 
     def get_reward_value(self, args) -> float:
